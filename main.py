881c37ae60bd9f79966ee2adb38f2925f062a64e
RainZhangSJ <rainzhang2000@hotmail.com>
2020-02-27 09:39:28 -0800

    camera code working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # CPEN 291 B_G11 Project 1
 #
 # Authors:
-# Date: 
+# Date:
 #
 
 #----------------------#
@@ -27,12 +27,13 @@ from time import sleep
 # Camera code
 #
 # Source: https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/7
-
+"""
 # image setup
 camera.resolution = (2592, 1944)
 camera.framerate = 15
 camera.annotate_text_size = 50
-
+"""
+camera = PiCamera()
 # take 5 photos
 camera.start_preview()
 camera.annotate_text = "B_G11"
@@ -40,7 +41,7 @@ for i in range(5):
     sleep(5)
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 camera.stop_preview()
-
+"""
 # take a video
 camera.start_preview()
 camera.start_recording('/home/pi/Desktop/video.h264')
@@ -48,9 +49,8 @@ sleep(5)
 camera.stop_recording()
 camera.stop_preview()
 
-
+"""
 
 #----------------------#
 # Sensors code
-
 
