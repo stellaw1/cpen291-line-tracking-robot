8f98d4a1f0f4eb02a27e9b2d06d976b4a26a9956
stellaw1 <stella01wang@gmail.com>
2020-02-27 08:25:23 -0800

    initialized main

---
+++ main.py
@@ -0,0 +1,27 @@
+# CPEN 291 B_G11 Project 1
+#
+# Authors:
+# Date: 
+#
+
+#----------------------#
+# 2WD Mobile Platform Code
+
+
+#----------------------#
+# RPi code
+
+
+#----------------------#
+# Line tracking code
+
+
+#----------------------#
+# Camera code
+
+
+
+#----------------------#
+# Sensors code
+
+
