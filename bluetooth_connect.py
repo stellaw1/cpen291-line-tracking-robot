79fce36076384d2992076d678ef049164c0bb33d
Amir Barkam <amirbarkam6@gmail.com>
2020-03-03 09:06:59 -0800

    Add code to connect to device

--- bluetooth_connect.py
+++ bluetooth_connect.py
@@ -3,14 +3,21 @@ import bluetooth
 target_name = "Amir's S9"
 target_address = None
 
-nearby_devices = bluetooth.discover_devices()
+port = 1
 
-for bdaddr in nearby_devices:
-    if target_name == bluetooth.lookup_name( bdaddr ):
-        target_address = bdaddr
-        break
+while (target_address == None):
 
-if target_address is not None:
-    print("found target bluetooth device with address %s", target_address)
-else:
-    print("could not find target bluetooth device nearby")
+    nearby_devices = bluetooth.discover_devices()
+
+    for bdaddr in nearby_devices:
+        if target_name == bluetooth.lookup_name( bdaddr ):
+            target_address = bdaddr
+
+print("Found target bluetooth device with address ", target_address)
+
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect(target_address, port)
+
+sock.send("hello!")
+
+sock.close()
