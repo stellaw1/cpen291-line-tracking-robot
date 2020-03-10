a94cd70542b58d298e3750ae2d2ae40384b51500
RainZhangSJ <rainzhang2000@hotmail.com>
2020-03-10 15:43:39 -0700

    Fixed errors in client.py

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-import 
 import glob
 import time
 from bluetooth import *
@@ -19,38 +18,38 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 advertise_service( server_sock, "LineTrackerServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-                    )
-while True:          
-	print "Waiting for connection on RFCOMM channel %d" % port
+                   profiles = [ SERIAL_PORT_PROFILE ],
+                   )
+while True:
+    print("Waiting for connection on RFCOMM channel ", port)
 
-	client_sock, client_info = server_sock.accept()
-	print "Accepted connection from ", client_info
+    client_sock, client_info = server_sock.accept()
+    print ("Accepted connection from ", client_info)
 
-	try:
+    try:
         data = client_sock.recv(1024)
-        if len(data) == 0: 
+        if len(data) == 0:
             break
-       
+
         motor_vals = get_data(data)
         speeds = get_speeds(motor_vals)
 
-		left_speed = speeds[0]
+        left_speed = speeds[0]
         right_speed = speeds[1]
 
         print(left_speed, right_speed)
 
         motor.robot_move(left_speed, right_speed, 0.1)
-        
-	except IOError:
-		pass
 
-	except KeyboardInterrupt:
-		print "disconnected"
-		client_sock.close()
-		server_sock.close()
-		print "all done"
-		break
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+        print("disconnected")
+        client_sock.close()
+        server_sock.close()
+        print("all done")
+        break
 
     def get_data(data):
         tup = tuple(filter(None, data.split(',')))
@@ -72,5 +71,5 @@ while True:
         else:
             left_speed = MAX_BACKWARDS
             right_speed = ((angle - 270) % 91) / 90 * MAX_BACKWARDS
-        
+
         return (left_speed, right_speed)
